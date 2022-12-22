97c8837e038ca9afae38f0a4a0eafa6097993101
candiduslynx <candiduslynx@gmail.com>
2022-12-22 14:23:34 +0200

    use example from apiary

--- snyk/dependencies_test.go
+++ snyk/dependencies_test.go
@@ -1,10 +1,9 @@
 package snyk
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -13,53 +12,91 @@ func TestDependencies_List(t *testing.T) {
 	setup()
 	defer teardown()
 
+	mux.HandleFunc("/org/long-uuid/dependencies", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method)
+
+		_, _ = fmt.Fprintf(w, `
+{
+  "results": [
+    {
+      "id": "gulp@3.9.1",
+      "name": "gulp",
+      "version": "3.9.1",
+      "latestVersion": "4.0.0",
+      "latestVersionPublishedDate": "2018-01-01T01:29:06.863Z",
+      "firstPublishedDate": "2013-07-04T23:27:07.828Z",
+      "isDeprecated": false,
+      "deprecatedVersions": [
+        "0.0.1",
+        "0.0.2",
+        "0.0.3"
+      ],
+      "licenses": [
+        {
+          "id": "snyk:lic:npm:gulp:MIT",
+          "title": "MIT license",
+          "license": "MIT"
+        }
+      ],
+      "dependenciesWithIssues": [
+        "minimatch@2.0.10",
+        "minimatch@0.2.14"
+      ],
+      "type": "npm",
+      "projects": [
+        {
+          "name": "atokeneduser/goof",
+          "id": "6d5813be-7e6d-4ab8-80c2-1e3e2a454545"
+        }
+      ],
+      "copyright": [
+        "Copyright (c) 2013-2018 Blaine Bublitz <blaine.bublitz@gmail.com>",
+        "Copyright (c) Eric Schoffstall <yo@contra.io> and other contributors"
+      ]
+    }
+  ],
+  "total": 1
+}`)
+	})
+
 	expected := []Dependency{
 		{
-			ID:                         "dep1",
-			Name:                       "dep-name",
-			Type:                       "abc",
-			Version:                    "2",
-			LatestVersion:              "3",
-			LatestVersionPublishedDate: ptr(time.Now().UTC()),
-			FirstPublishedDate:         ptr(time.Now().UTC()),
-			IsDeprecated:               true,
-			DeprecatedVersions:         []string{"1", "2"},
-			DependenciesWithIssues:     []string{"a", "b"},
-			IssuesCritical:             1,
-			IssuesHigh:                 2,
-			IssuesMedium:               3,
-			IssuesLow:                  4,
-			Licenses:                   nil,
+			ID:                         "gulp@3.9.1",
+			Name:                       "gulp",
+			Type:                       "npm",
+			Version:                    "3.9.1",
+			LatestVersion:              "4.0.0",
+			LatestVersionPublishedDate: ptr(mustParseTime(t, "2018-01-01T01:29:06.863Z")),
+			FirstPublishedDate:         ptr(mustParseTime(t, "2013-07-04T23:27:07.828Z")),
+			IsDeprecated:               false,
+			DeprecatedVersions: []string{
+				"0.0.1",
+				"0.0.2",
+				"0.0.3",
+			},
+			DependenciesWithIssues: []string{
+				"minimatch@2.0.10",
+				"minimatch@0.2.14",
+			},
+			Licenses: []DependencyLicense{
+				{
+					ID:      "snyk:lic:npm:gulp:MIT",
+					Title:   "MIT license",
+					License: "MIT",
+				},
+			},
 			Projects: []Project{
 				{
-					ID:   "proj1",
-					Name: "proj-name",
+					ID:   "6d5813be-7e6d-4ab8-80c2-1e3e2a454545",
+					Name: "atokeneduser/goof",
 				},
 			},
 			Copyright: []string{
-				"some",
-				"license",
+				"Copyright (c) 2013-2018 Blaine Bublitz <blaine.bublitz@gmail.com>",
+				"Copyright (c) Eric Schoffstall <yo@contra.io> and other contributors",
 			},
 		},
 	}
-	mux.HandleFunc("/org/long-uuid/dependencies", func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, http.MethodPost, r.Method)
-		b, err := json.Marshal(
-			dependenciesRoot{
-				Total:   1,
-				Results: expected,
-			},
-		)
-		if err != nil {
-			http.Error(w, "unable to marshal response: "+err.Error(), http.StatusBadRequest)
-			return
-		}
-		if _, err := w.Write(b); err != nil {
-			http.Error(w, "failed to write", http.StatusBadRequest)
-			return
-		}
-	})
-
 	actual, _, err := client.Dependencies.List(ctx, "long-uuid")
 
 	assert.NoError(t, err)
--- snyk/util_test.go
+++ snyk/util_test.go
@@ -1,5 +1,18 @@
 package snyk
 
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
 func ptr[A any](a A) *A {
 	return &a
+}
+
+func mustParseTime(t *testing.T, s string) time.Time {
+	timestamp, err := time.Parse(time.RFC3339, s)
+	assert.NoError(t, err)
+	return timestamp
 }
