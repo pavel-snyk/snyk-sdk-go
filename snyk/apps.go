4b00e92fa81c6f11a87da6fe837006f078426c96
Pavel Sorokin <60606414+pavel-snyk@users.noreply.github.com>
2026-01-30 23:19:41 +0100

    feat(apps): implement methods to create/delete app_installs

--- snyk/apps.go
+++ snyk/apps.go
@@ -2,6 +2,7 @@ package snyk
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
@@ -21,6 +22,16 @@ type AppsServiceAPI interface {
 	//
 	// See: https://docs.snyk.io/snyk-api/reference/apps#get-orgs-org_id-apps-installs
 	ListAppInstallsForOrg(ctx context.Context, orgID string, opts *ListAppInstallOptions) ([]AppInstall, *Response, error)
+
+	// CreateAppInstallForOrg installs a Snyk App to an Organization. The App must use unattended authentication e.g. client credentials.
+	//
+	// See: https://docs.snyk.io/snyk-api/reference/apps#post-orgs-org_id-apps-installs
+	CreateAppInstallForOrg(ctx context.Context, orgID, appID string) (*AppInstall, *Response, error)
+
+	// DeleteAppInstallFromOrg revokes app authorization for an Organization with install ID.
+	//
+	// See: https://docs.snyk.io/snyk-api/reference/apps#delete-orgs-org_id-apps-installs-install_id
+	DeleteAppInstallFromOrg(ctx context.Context, orgID, appInstallID string) (*Response, error)
 }
 
 // AppsService handles communication with the app related methods of the Snyk API.
@@ -71,6 +82,12 @@ type ListAppInstallOptions struct {
 	Expand string `url:"expand,omitempty"`
 }
 
+type appInstallRoot struct {
+	AppInstalls *AppInstall `json:"data"`
+}
+
+func (air appInstallRoot) String() string { return Stringify(air) }
+
 type appInstallsRoot struct {
 	AppInstalls []AppInstall `json:"data"`
 }
@@ -78,6 +95,10 @@ type appInstallsRoot struct {
 func (ai AppInstall) String() string { return Stringify(ai) }
 
 func (s *AppsService) ListAppInstallsForOrg(ctx context.Context, orgID string, opts *ListAppInstallOptions) ([]AppInstall, *Response, error) {
+	if orgID == "" {
+		return nil, nil, errors.New("failed to list app installs for org: org id must be supplied")
+	}
+
 	if opts == nil {
 		opts = &ListAppInstallOptions{Expand: "app"}
 	}
@@ -100,4 +121,71 @@ func (s *AppsService) ListAppInstallsForOrg(ctx context.Context, orgID string, o
 	}
 
 	return root.AppInstalls, resp, nil
+}
+
+func (s *AppsService) CreateAppInstallForOrg(ctx context.Context, orgID, appID string) (*AppInstall, *Response, error) {
+	if orgID == "" {
+		return nil, nil, errors.New("failed to create app install for org: org id must be supplied")
+	}
+	if appID == "" {
+		return nil, nil, errors.New("failed to create app install for org: app id must be supplied")
+	}
+
+	opts := &ListOptions{BaseOptions: BaseOptions{Version: appsAPIVersion}}
+	path, err := addOptions(fmt.Sprintf("orgs/%v/%v/installs", orgID, appsBasePath), opts)
+	if err != nil {
+		return nil, nil, err
+	}
+	// inline jsonapi create payload to keep create function simple
+	var createRequest struct {
+		Data struct {
+			Type string `json:"type"`
+		} `json:"data"`
+		Relationships struct {
+			App struct {
+				Data struct {
+					ID   string `json:"id"`
+					Type string `json:"type"`
+				} `json:"data"`
+			} `json:"app"`
+		} `json:"relationships"`
+	}
+	createRequest.Data.Type = "app_install"
+	createRequest.Relationships.App.Data.ID = appID
+	createRequest.Relationships.App.Data.Type = "app"
+
+	req, err := s.client.prepareRequest(ctx, http.MethodPost, s.client.restBaseURL, path, createRequest)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	appInstallRoot := new(appInstallRoot)
+	resp, err := s.client.do(ctx, req, &appInstallRoot)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return appInstallRoot.AppInstalls, resp, nil
+}
+
+func (s *AppsService) DeleteAppInstallFromOrg(ctx context.Context, orgID, appInstallID string) (*Response, error) {
+	if orgID == "" {
+		return nil, errors.New("failed to delete app install for org: org id must be supplied")
+	}
+	if appInstallID == "" {
+		return nil, errors.New("failed to delete app install for org: app install id must be supplied")
+	}
+
+	opts := &ListOptions{BaseOptions: BaseOptions{Version: appsAPIVersion}}
+	path, err := addOptions(fmt.Sprintf("orgs/%v/%v/installs/%v", orgID, appsBasePath, appInstallID), opts)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := s.client.prepareRequest(ctx, http.MethodDelete, s.client.restBaseURL, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.do(ctx, req, nil)
 }
